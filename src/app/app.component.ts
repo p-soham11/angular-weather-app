6ac5c01865c50cc87177ac5b1e8e8d78715bab0a
p-soham11 <sohampal1108@gmail.com>
2024-08-08 22:17:16 +0530

    Adding Search Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { WeatherService } from '../services/weather.service';
 import { WeatherData } from './models/weather.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,13 +16,25 @@ export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
   weatherData?: WeatherData;
+  // Default city is set to Kolkata
+  city: string = 'Kolkata';
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Kolkata').subscribe({
+    this.getWeatherData(this.city);
+    this.city = '';
+  }
+  title = 'weatherApp';
+
+  onSubmit() {
+    this.getWeatherData(this.city);
+    this.city = '';
+  }
+
+  getWeatherData(city: string) {
+    this.weatherService.getWeatherData(city).subscribe({
       next: (response) => {
         this.weatherData = response;
       },
     });
   }
-  title = 'weatherApp';
 }
